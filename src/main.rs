83a03cd3781697d0a3cbe0a73814e95f3ec5f0a2
chesedo <pieter@chesedo.me>
2024-03-23 17:04:35 +0000

    refactor: use tower layer

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,15 @@ use axum::{
     http::{request::Parts, StatusCode},
     response::{IntoResponse, Response},
     routing::{get, post},
-    Json, RequestPartsExt, Router,
+    Json, Router,
 };
-use axum_extra::{
-    headers::{authorization::Bearer, Authorization},
-    TypedHeader,
-};
-use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
+use jsonwebtoken::{encode, DecodingKey, EncodingKey, Header, Validation};
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::fmt::Display;
 use std::time::SystemTime;
+use tower_jwt::{JwtLayer, RequestClaim};
 
 static KEYS: Lazy<Keys> = Lazy::new(|| {
     // note that in production, you will probably want to use a random SHA-256 hash or similar
@@ -28,7 +25,11 @@ async fn main() -> shuttle_axum::ShuttleAxum {
     let app = Router::new()
         .route("/public", get(public))
         .route("/private", get(private))
-        .route("/login", post(login));
+        .route("/login", post(login))
+        .layer(JwtLayer::<Claims>::new(
+            Validation::default(),
+            KEYS.decoding.clone(),
+        ));
 
     Ok(app.into())
 }
@@ -103,16 +104,13 @@ where
     type Rejection = AuthError;
 
     async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
-        // Extract the token from the authorization header
-        let TypedHeader(Authorization(bearer)) = parts
-            .extract::<TypedHeader<Authorization<Bearer>>>()
-            .await
-            .map_err(|_| AuthError::InvalidToken)?;
-        // Decode the user data
-        let token_data = decode::<Claims>(bearer.token(), &KEYS.decoding, &Validation::default())
-            .map_err(|_| AuthError::InvalidToken)?;
+        let claim = parts.extensions.get::<RequestClaim<Claims>>();
 
-        Ok(token_data.claims)
+        if let Some(claim) = claim {
+            Ok(claim.claim.clone())
+        } else {
+            Err(AuthError::MissingCredentials)
+        }
     }
 }
 
@@ -123,7 +121,6 @@ impl IntoResponse for AuthError {
             AuthError::WrongCredentials => (StatusCode::UNAUTHORIZED, "Wrong credentials"),
             AuthError::MissingCredentials => (StatusCode::BAD_REQUEST, "Missing credentials"),
             AuthError::TokenCreation => (StatusCode::INTERNAL_SERVER_ERROR, "Token creation error"),
-            AuthError::InvalidToken => (StatusCode::BAD_REQUEST, "Invalid token"),
         };
         let body = Json(json!({
             "error": error_message,
@@ -148,7 +145,7 @@ impl Keys {
 }
 
 // the JWT claim
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 struct Claims {
     sub: String,
     company: String,
@@ -175,5 +172,4 @@ enum AuthError {
     WrongCredentials,
     MissingCredentials,
     TokenCreation,
-    InvalidToken,
 }
